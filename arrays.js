ceec209ee752344ddd4bf49fdbfaeda0ab7cf30b
karly.mareka@gmail.com <karly.mareka@gmail.com>
2018-05-20 18:13:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,8 +9,8 @@ function addElementToBeginningOfArray(array, element) {
   return array;
 }
 
-
 function destructivelyAddElementToBeginningOfArray(array, element) {
     array.unshift(element)
     return array;
 }
+
